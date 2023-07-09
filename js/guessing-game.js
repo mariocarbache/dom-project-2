1f0452844a34fcbf98c0c49eff258263470bcfcf
Mario Carbache <mariocarbache@gmail.com>
2023-07-09 19:42:57 -0400

    updated javascript file DOM project

--- js/guessing-game.js
+++ js/guessing-game.js
@@ -5,3 +5,98 @@ by running "npm run test".
 In this file, you will also include the event listeners that are needed to interact with your HTML file when
 a user clicks a button or adds a guess to the input field.
 */
+
+function generateWinningNumber() {
+    return Math.floor(Math.random() * 100) + 1;
+}
+
+function shuffle (arr)
+{
+    for (let i = arr.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
+}
+
+class Game {
+    constructor(){
+        this.playersGuess = null;
+        this.pastGuesses = [];
+        this.winningNumber = generateWinningNumber();
+    }
+
+    difference() {
+        return Math.abs(this.playersGuess-this.winningNumber);
+    }
+
+    isLower(){
+        if (this.playersGuess<this.winningNumber){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    playersGuessSubmission(number){
+        let message = "";
+        function isValid(num){
+            if((num < 1 || num > 100 || isNaN(num))){
+                throw new Error ("Invalid Input");
+            }
+        }
+        try{
+            isValid(number);
+            this.playersGuess = number;
+            message = this.checkGuess();
+        }catch(e){
+            console.error(e);
+        }
+        return message;
+    }
+
+    checkGuess(){
+        let message = "";
+        if(this.pastGuesses.includes(this.playersGuess)){
+            message = "You have already guessed that number.";
+        }else{    
+        if(this.playersGuess==this.winningNumber && this.pastGuesses.length != 5){
+            this.pastGuesses.push(this.playersGuess)
+            message = "You Win!";
+        }else if(this.difference()<10 && this.pastGuesses.length != 5){
+            this.pastGuesses.push(this.playersGuess)
+            message = "You're burning up!";
+        }else if(this.difference()<25 && this.pastGuesses.length != 5){
+            this.pastGuesses.push(this.playersGuess)
+            message = "You're lukewarm.";
+        }else if(this.difference()<50 && this.pastGuesses.length != 5){
+            this.pastGuesses.push(this.playersGuess)
+            message = "You're a bit chilly.";
+        }
+        else if(this.difference()<75 && this.pastGuesses.length != 5){
+            this.pastGuesses.push(this.playersGuess)
+            message = "You're ice cold!";
+        }
+        else if(this.pastGuesses.length==5 && this.playersGuess!=this.winningNumber){
+            message = "You Lose";
+        }
+    }
+        return message;
+    }
+
+    newGame(){
+        let g2 = new Game();
+        return g2;
+    }
+
+    provideHint(){
+        let hints = [];
+        hints.push(this.winningNumber);
+        hints.push(generateWinningNumber());
+        hints.push(generateWinningNumber());
+        shuffle(hints);
+        return hints;
+    }
+}
+
+let g1 = new Game();
